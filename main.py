79acf845f40c268de2ec96a14d128edad2553c50
KevinSteele2 <kevin.steele@outlook.com>
2026-02-07 17:00:54 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# TO activate venv type lastfm/Scripts/activate in terminal
+import requests
+API_KEY = "0472b186832ba248f24acf8f554113df"
+API_SECRET = "572a702051f64a54b75d8a3f552f1437"
+API_USERNAME = "Donaldddddd"
+
