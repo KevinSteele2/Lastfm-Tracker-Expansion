e6b58db8671929a1755d391c5713cd5d460e5e50
KevinSteele2 <kevin.steele@outlook.com>
2026-02-26 22:50:38 -0800

    added gitignore and made it so api key and secret not visible

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 # TO activate venv type lastfm/Scripts/activate in terminal
 import requests
-API_KEY = "0472b186832ba248f24acf8f554113df"
-API_SECRET = "572a702051f64a54b75d8a3f552f1437"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+API_SECRET = os.getenv("API_SECRET")
 USERNAME = "Donaldddddd"
 
 def test_user_info():
