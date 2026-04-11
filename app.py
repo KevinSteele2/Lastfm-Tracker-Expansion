a3c60b8e56c23730415957dbcb0401dbc465eb96
KevinSteele2 <kevin.steele@outlook.com>
2026-04-10 18:38:17 -0700

    made room separate, links to lastfm

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from main import get_all_scrobbles, group_by_album, album_play_counts, load_cach
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/lastfm')
 def index():
-    return render_template('room.html')
+    return render_template('lastfm.html')
 
 @app.route('/api/albums')
 def get_albums():
