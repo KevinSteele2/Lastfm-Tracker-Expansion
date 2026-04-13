3e4710b81d67cc1c57aaacf984d29cd13e2bdf1c
KevinSteele2 <kevin.steele@outlook.com>
2026-04-12 18:11:57 -0700

    fixed arrow bug

--- static/room.js
+++ static/room.js
@@ -7,7 +7,7 @@ function turn(direction) {
     current = (current + direction + 4) % 4;
     rotation += direction * 90;
     document.getElementById(`wall-${wallOrder[current]}`).classList.add('active');
-    document.getElementById('room').style.transform = `rotateY(${rotation}deg)`;
+    document.getElementById('room').style.transform = `translate(-50%, -50%) rotateY(${rotation}deg)`;
 }
 
 document.getElementById('wall-front').classList.add('active');
